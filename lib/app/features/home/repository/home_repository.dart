c1c0da1969027f9bfda4026f0249e2a407a67806
Gabriel Souza <gabriel12saraiva@gmail.com>
2022-08-20 16:17:11 -0300

    Fixes an error handling issue

--- lib/app/features/home/repository/home_repository.dart
+++ lib/app/features/home/repository/home_repository.dart
@@ -7,12 +7,17 @@ class HomeRepository {
   final HttpClient httpClient = HttpClientImp();
 
   Future<List<OfferModel>> getOffers() async {
-    final ResponseModel response = await httpClient.get('/cooperative-offers');
 
-    if (!response.isOk || response.body == null) throw AppError.api;
-    
-    return (response.body?['items'] as List)
-        .map((item) => OfferModel.fromJson(item))
-        .toList();
+    try {
+      final ResponseModel response = await httpClient.get('/cooperative-offers');
+
+      if (!response.isOk || response.body == null) throw AppError.api;
+      
+      return (response.body?['items'] as List)
+          .map((item) => OfferModel.fromJson(item))
+          .toList();
+    } catch (e) {
+      rethrow;
+    }
   }
 }

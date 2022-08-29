e6d9f1fa3f2af8e28c33a1ccc6c660af2fdacbb4
Gabriel Souza <gabriel12saraiva@gmail.com>
2022-08-29 00:19:20 -0300

    Creates the tests for increaseValue and decreaseValue methods

--- test/home_bloc_test.dart
+++ test/home_bloc_test.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:frontend/app/app.dart';
 import 'package:frontend/app/features/home/blocs/blocs.dart';
@@ -5,6 +6,8 @@ import 'package:frontend/app/features/home/repository/repository.dart';
 import 'package:mocktail/mocktail.dart';
 
 class HomeRepositoryMock extends Mock implements HomeRepository {}
+
+class BuildContextMock extends Mock implements BuildContext {}
 
 void main() {
 
@@ -42,7 +45,8 @@ void main() {
     
     test(
       'When adding the loading event '
-      'must emit OfferListStateLoading and OfferListStateLoaded in this exact order', () async {
+      'must emit OfferListStateLoaded after OfferListStateLoading, '
+      'with OfferModels in the offerList property', () async {
       
       when(() => repository.getOffers())
           .thenAnswer((_) async => <OfferModel>[offer, offer]
@@ -57,15 +61,20 @@ void main() {
           isA<OfferListStateLoaded>(), 
         ])
       );
+
+      expect(bloc.state.offerList, allOf([
+        isA<List<OfferModel>>(),
+        isNotEmpty
+      ]));
     });
 
     test(
       'When the getOffers method returns empty '
-      'must emit OfferListStateEmpty after OfferListStateLoading', () async {
+      'must emit OfferListStateEmpty after OfferListStateLoading, '
+      'with empty offerList property', () async {
       
       when(() => repository.getOffers())
-          .thenAnswer((_) async => <OfferModel>[]
-          );
+          .thenAnswer((_) async => <OfferModel>[]);
 
       bloc.add(OfferListEventLoading());
 
@@ -76,6 +85,11 @@ void main() {
           isA<OfferListStateEmpty>(), 
         ])
       );
+
+      expect(bloc.state.offerList, allOf([
+        isA<List<OfferModel>>(),
+        isEmpty
+      ]));
     });
 
     test(
@@ -101,7 +115,7 @@ void main() {
 
     test(
       'When catch an AppError.timeout '
-      'must emit OfferListStateError after OfferListStateLoading'
+      'must emit OfferListStateError after OfferListStateLoading, '
       'with the text [Verifique sua conexão com a internet]', () async {
       
       when(() => repository.getOffers())
@@ -122,7 +136,7 @@ void main() {
     
     test(
       'When catch an AppError.api '
-      'must emit OfferListStateError after OfferListStateLoading'
+      'must emit OfferListStateError after OfferListStateLoading, '
       'with the text [Houve um erro na comunicação com o servidor]', () async {
       
       when(() => repository.getOffers())
@@ -143,7 +157,7 @@ void main() {
 
     test(
       'When catch an unmapped error '
-      'must emit OfferListStateError after OfferListStateLoading'
+      'must emit OfferListStateError after OfferListStateLoading, '
       'with the text [Ops, algo deu errado]', () async {
       
       when(() => repository.getOffers())
@@ -160,6 +174,67 @@ void main() {
       );
 
       expect(bloc.state.error, equals('Ops, algo deu errado'));
+    });
+  });
+
+  group('HomeBloc Methods |', () {
+
+    setUp(() {
+      bloc.valueOfEnergyAcount = 0.0;
+    });
+
+    testWidgets(
+      '[increaseValue] must increment the valueOfEnergyAcount with the value parameter '
+      'and must update the [valueController.text]', (WidgetTester tester) async {
+      await tester.pumpWidget(
+        Builder(
+          builder: (BuildContext context) {
+
+            const String space = '\u{00A0}';
+
+            for (var i = 0; i < 3; i++) {
+              bloc.increaseValue(5, context);
+            }
+
+            expect(bloc.valueOfEnergyAcount, 15.0);
+            expect(bloc.valueController.text, 'R\$${space}15,00');
+
+            return const Placeholder();
+          },
+        ),
+      );
+    });
+
+    testWidgets(
+      '[decreaseValue] must decrement the valueOfEnergyAcount with the value parameter '
+      'up to 0, and must update the [valueController.text]', (WidgetTester tester) async {
+
+      bloc.valueOfEnergyAcount = 20.0;
+
+      await tester.pumpWidget(
+        Builder(
+          builder: (BuildContext context) {
+
+            const String space = '\u{00A0}';
+
+            for (var i = 0; i < 3; i++) {
+              bloc.decreaseValue(5, context);
+            }
+
+            expect(bloc.valueOfEnergyAcount, 5.0);
+            expect(bloc.valueController.text, 'R\$${space}5,00');
+
+            for (var i = 0; i < 3; i++) {
+              bloc.decreaseValue(5, context);
+            }
+
+            expect(bloc.valueOfEnergyAcount, 0.0);
+            expect(bloc.valueController.text, 'R\$${space}0,00');
+
+            return const Placeholder();
+          },
+        ),
+      );
     });
   });
 }

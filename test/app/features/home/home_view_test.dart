f79563b240393fdd36e198c522d5c6ad6ba41c5d
Gabriel Souza <gabriel12saraiva@gmail.com>
2022-09-01 21:07:21 -0300

    Updates the HomeView test

--- test/app/features/home/home_view_test.dart
+++ test/app/features/home/home_view_test.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:frontend/app/app.dart';
+import 'package:frontend/app/features/home/blocs/blocs.dart';
 import 'package:frontend/app/features/home/repository/home_repository.dart';
 import 'package:mocktail/mocktail.dart';
 
@@ -8,18 +9,18 @@ class HomeRepositoryMock extends Mock implements HomeRepository {}
 
 void main() {
   
-  // late final HomeBloc bloc;
-  // late final HomeRepository repository; 
-  // setUp(() {
-  //   repository = HomeRepositoryMock();
-  //   bloc = HomeBloc(repository: repository);
-  // });
+  late final HomeBloc bloc;
+  late final HomeRepository repository; 
+  setUp(() {
+    repository = HomeRepositoryMock();
+    bloc = HomeBloc(repository: repository);
+  });
 
   group('HomeView |', () {
     testWidgets('Should exist the widgets structure of the HomeView', (tester) async {
       await tester.pumpWidget(
-        const MaterialApp(
-          home: HomeView(),
+        MaterialApp(
+          home: HomeView(bloc: bloc),
         )
       );
 

48f1f2ad109206b6cf39e5edddba244e68eebfda
Ramon Bosi <ramombosi2015@gmail.com>
2022-09-06 19:45:54 -0300

    Analisando o valor mensal do cliente com as coorperativas

--- frontend-wattio/src/pages/CompararCoorperativas/index.js
+++ frontend-wattio/src/pages/CompararCoorperativas/index.js
@@ -1,7 +1,55 @@
+import { useEffect,useContext, useState } from 'react'
+import { ContextoCliente } from '@store/ContextoCliente'
+import coorperativas from '@services/coorperativas.json'
+import Container from 'react-bootstrap/Container';
+
 export default function CompararCoorperativas(){
+
+  const { pegarDadosCliente } = useContext(ContextoCliente)
+  const [carregarCoorperativas, setCarregarCoorperativas] = useState(<></>)
+  const dadosCliente = pegarDadosCliente()
+
+  useEffect(() =>{
+    //dadosCliente = {nomeEmpresa: '', valorMensal: '', tipoCliente: ''} 
+
+    const analisarCoorperativas = coorperativas.filter((dc) =>{
+      if((dadosCliente.valorMensal >= dc.valorMinMensal && dadosCliente.valorMensal <= dc.valorMaxMensal) && (dadosCliente.tipoCliente === dc.tipoCliente)){
+        return dc
+      }
+    })
+
+    const maiorDesconto = analisarCoorperativas.map((des) =>{
+      return des.desconto
+    }).sort().reverse()[0]
+
+    const criarComponentes = analisarCoorperativas.map((coor)=>{
+      return (
+        <div key={coor.id} id={coor.id} className={`border ms-2 ${coor.desconto === maiorDesconto ? 'border-primary order-first' : ''}`}>
+          <p>Nome: {coor.nome}</p>
+          <p>Desconto: {coor.desconto * 100}%</p>
+          <p>Valor mensal com desconto: {dadosCliente.valorMensal * (1 - coor.desconto)}</p>
+          <p>Valor anual com desconto: {(dadosCliente.valorMensal * (1 - coor.desconto)) * 12}</p>
+          <p>Tipo cliente: {coor.tipoCliente}</p>
+        </div>
+      )
+    })
+
+    setCarregarCoorperativas(criarComponentes)
+  },[])
+
   return(
-    <>
-      <h1>Rota Comparar Coorperativas</h1>
-    </>
+    <Container>
+      <div>
+        <h2>Dados do cliente</h2>
+        <p>Nome: {dadosCliente.nomeEmpresa}</p>
+        <p>Valor mensal: R${dadosCliente.valorMensal}</p>
+        <p>Valor anual: R${dadosCliente.valorMensal * 12}</p>
+        <p>Tipo cliente: {dadosCliente.tipoCliente}</p>
+      </div>
+      <h2>Coorperativas</h2>
+      <div className='d-flex'>
+        {carregarCoorperativas}
+      </div>
+    </Container>
   )
 }
--- frontend-wattio/src/pages/Home/validacaoForm/index.js
+++ frontend-wattio/src/pages/Home/validacaoForm/index.js
@@ -2,5 +2,5 @@ import * as yup from 'yup'
 
 export const schema = yup.object({
   nomeEmpresa: yup.string().max(1500, 'Máximo de 1500 caracteres').required('Nome/Empresa é obrigatório'),
-  valorMensal: yup.number().typeError('Valor mensal é obrigatório').min(1000,'Valor mínimo é R$1.000').required('Valor mensal é obrigatório')
+  valorMensal: yup.number().typeError('Valor mensal é obrigatório').min(1000,'Valor mínimo é R$1.000').max(100000, 'Valor máximo é R$100.000').required('Valor mensal é obrigatório')
 }).required()

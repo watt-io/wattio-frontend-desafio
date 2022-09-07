e58454d8fc5c5b1fc5e061b8afa690ee06666d71
Ramon Bosi <ramombosi2015@gmail.com>
2022-09-07 14:48:22 -0300

    Estilizando o formulário dos dados do cliente

--- frontend-wattio/src/pages/Home/index.js
+++ frontend-wattio/src/pages/Home/index.js
@@ -23,32 +23,32 @@ export default function Home(){
 
   return(
     <Container>
-      <Form className="mt-4" onSubmit={handleSubmit(procurarCoorperativas)}>
+      <Form className="mt-4 p-3 rounded-3" onSubmit={handleSubmit(procurarCoorperativas)} style={{backgroundColor: 'rgba(5, 5, 130, .8)'}}>
       <Form.Group className="mb-3" controlId="formBasicEmpresa">
-        <Form.Label>*Nome/Empresa</Form.Label>
+        <Form.Label className='text-light'>*Nome/Empresa</Form.Label>
         <Form.Control type="text" {...register('nomeEmpresa')}/>
         <Form.Text className="text-muted">
-          <p className='m-0 mt-1 text-danger'>{errors.nomeEmpresa?.message}</p>
+          <p className='m-0 mt-1 text-danger fw-bold fs-6'>{errors.nomeEmpresa?.message}</p>
         </Form.Text>
       </Form.Group>
       <Form.Group className="mb-3" controlId="formBasicValorMensal">
-      <Form.Label>*Valor médio mensal de energia da minha casa/empresa</Form.Label>
+      <Form.Label className='text-light'>*Valor médio mensal de energia da minha casa/empresa</Form.Label>
       <InputGroup>
         <InputGroup.Text id="valorMensal">R$</InputGroup.Text>
         <Form.Control {...register('valorMensal')} type="number" aria-label="valorMensal" aria-describedby="valorMensal"/>
       </InputGroup>
       <Form.Text className="text-muted">
-        <p className='m-0 mt-1 text-danger'>{errors.valorMensal?.message}</p>
+        <p className='m-0 mt-1 text-danger fw-bold fs-6'>{errors.valorMensal?.message}</p>
       </Form.Text>
       </Form.Group>
       <Form.Group>
-        <Form.Label className='me-3'>*Tipo de cliente:</Form.Label>
+        <Form.Label className='me-3 text-light'>*Tipo de cliente:</Form.Label>
         <Form.Select aria-label="Tipo de cliente" {...register('tipoCliente')}>
           <option value="PF">PF</option>
           <option value="PJ">PJ</option>
         </Form.Select>
       </Form.Group>
-      <Button variant="primary" type="submit" className="mt-3">Procurar coorperativas</Button>
+      <Button variant="warning" type="submit" className="mt-3">Procurar coorperativas</Button>
     </Form>
     </Container>
   )

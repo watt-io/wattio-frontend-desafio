0f5040ff9a9a5771ff3a3d7707fb8048d779b000
Caio Dantas <caiodantas26@hotmail.com>
2024-01-08 16:17:33 -0300

    desafio finalizado

---
+++ lib/models/coop_model.dart
@@ -0,0 +1,14 @@
+class CoopModel {
+  String nome;
+  double valorMinimoMensal;
+  double valorMaximoMensal;
+  double desconto;
+
+
+  CoopModel({
+    required this.nome,
+    required this.valorMinimoMensal,
+    required this.valorMaximoMensal,
+    required this.desconto,
+  });
+}

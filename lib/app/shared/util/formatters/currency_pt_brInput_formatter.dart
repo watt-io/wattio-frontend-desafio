892f28e3a7b01a29bcfa44882feeda7f92bf56d5
offKevyn <kevynsilva42@gmail.com>
2024-01-13 15:55:37 -0300

    add: shared utils (Formatters, SnackBarCustom, BrazilianStates)

---
+++ lib/app/shared/util/formatters/currency_pt_brInput_formatter.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/services.dart';
+import 'package:intl/intl.dart';
+
+class CurrencyPtBrInputFormatter extends TextInputFormatter {
+  @override
+  TextEditingValue formatEditUpdate(
+      TextEditingValue oldValue, TextEditingValue newValue) {
+    if (newValue.selection.baseOffset == 0) {
+      return newValue;
+    }
+
+    double value = double.parse(newValue.text);
+    final formatter = NumberFormat("#,##0.00", "pt_BR");
+    String newText = "R\$ ${formatter.format(value / 100)}";
+
+    return newValue.copyWith(
+        text: newText,
+        selection: TextSelection.collapsed(offset: newText.length));
+  }
+}
